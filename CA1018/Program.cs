2965f7c49eaf1b32d5eda5bcad8b18356628e80b
Gergely Jakó <jako.gergely@verebelyszki.hu>
2021-10-18 14:49:50 +0200

    CA1018

--- CA1018/Program.cs
+++ CA1018/Program.cs
@@ -16,14 +16,27 @@ namespace CA1018
             {
                 while (!sr.EndOfStream) csincsillak.Add(new Chichilla(sr.ReadLine()));
             }
-            Console.WriteLine($"2. feladat:\nÖsszesen {csincsillak.Count} csincsilla van.\n");
+            Console.WriteLine($"2.feladat:\nÖsszesen {csincsillak.Count} csincsilla van.\n");
 
             var szereti = csincsillak.Count(x => x.Simi);
 
-            Console.WriteLine("3. feladat:\nA csincsillák {0:0.00}%-a szereti ha simogatják.\n ", (float)szereti / csincsillak.Count * 100);
+            Console.WriteLine("3.feladat:\nA csincsillák {0:0.00}%-a szereti ha simogatják.\n ", (float)szereti / csincsillak.Count * 100);
 
             var oscs = csincsillak.Where(x => x.Kor > 8 && x.Suly < 360).FirstOrDefault();
-            Console.WriteLine($"4.feladat:\n{(oscs != null ? "nincs ilyen csincsilla" : $"{oscs.Nev} {Math.Floor(oscs.Kor)} éves és {oscs.Suly}")}");
+            Console.WriteLine($"4.feladat:\n{(oscs is null ? "nincs ilyen csincsilla" : $"{oscs.Nev} {Math.Floor(oscs.Kor)} éves és {oscs.Suly}")}\n");
+
+            Console.WriteLine("5.feladat:");
+            var rend = csincsillak.OrderByDescending(x => x.Suly).ToList();
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("{0,-12} {1} g", rend[i].Nev, rend[i].Suly);
+            }
+
+            Console.WriteLine("\n6.feladat: ");
+            csincsillak
+                .GroupBy(x => x.Szul.Year)
+                .ToList()
+                .ForEach(x => Console.WriteLine($"{x.Key}: {x.Count()}"));
 
             Console.ReadKey(true);
         }
