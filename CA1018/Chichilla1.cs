e934e1c08cfbbabde5f7d036d46dfd626a7e8c67
Gergely Jakó <jako.gergely@verebelyszki.hu>
2021-10-18 14:37:24 +0200

    CA1018

--- CA1018/Chichilla1.cs
+++ CA1018/Chichilla1.cs
@@ -13,13 +13,14 @@ namespace CA1018
         public int Suly { get; set; }
         public bool Simi { get; set; }
 
+        public double Kor => (DateTime.Now - Szul).TotalDays / 365.25;
         public Chichilla(string r)
         {
             var t = r.Split(';');
             Nev = t[0];
             Szul = DateTime.Parse(t[1]);
             Suly = int.Parse(t[2]);
-            Simi = bool.Parse(t[3]);
+            Simi = t[3] == "I";
 
         }
     }
--- CA1018/Program.cs
+++ CA1018/Program.cs
@@ -18,7 +18,12 @@ namespace CA1018
             }
             Console.WriteLine($"2. feladat:\nÖsszesen {csincsillak.Count} csincsilla van.\n");
 
+            var szereti = csincsillak.Count(x => x.Simi);
 
+            Console.WriteLine("3. feladat:\nA csincsillák {0:0.00}%-a szereti ha simogatják.\n ", (float)szereti / csincsillak.Count * 100);
+
+            var oscs = csincsillak.Where(x => x.Kor > 8 && x.Suly < 360).FirstOrDefault();
+            Console.WriteLine($"4.feladat:\n{(oscs != null ? "nincs ilyen csincsilla" : $"{oscs.Nev} {Math.Floor(oscs.Kor)} éves és {oscs.Suly}")}");
 
             Console.ReadKey(true);
         }
