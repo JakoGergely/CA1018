230079166f48ea2d7fe4cdf1305c6db8f6e76188
Jakó Gergely <jako.gergely@verebely.dc>
2021-10-18 14:09:48 +0200

    Add project files.

---
+++ CA1018/Chichilla1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA1018
+{
+    class Chichilla
+    {
+        public string Nev { get; set; }
+        public DateTime Szul { get; set; }
+        public int Suly { get; set; }
+        public bool Simi { get; set; }
+
+        public Chichilla(string r)
+        {
+            var t = r.Split(';');
+            Nev = t[0];
+            Szul = DateTime.Parse(t[1]);
+            Suly = int.Parse(t[2]);
+            Simi = bool.Parse(t[3]);
+
+        }
+    }
+}
---
+++ CA1018/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA1018
+{
+    class Program
+    {
+        static List<Chichilla> csincsillak = new List<Chichilla>();
+        static void Main(string[] args)
+        {
+            using (var sr = new StreamReader(@"..\..\res\chi.txt", Encoding.UTF8))
+            {
+                while (!sr.EndOfStream) csincsillak.Add(new Chichilla(sr.ReadLine()));
+            }
+            Console.WriteLine($"2. feladat:\nÖsszesen {csincsillak.Count} csincsilla van.\n");
+
+
+            Console.ReadKey(true);
+        }
+    }
+}
